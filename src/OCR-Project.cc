05cabe49b26fbded1d4b8e55c0a17ad2c07c37c5
jakubo87 <jakub_o@hotmail.com>
2018-09-10 20:23:42 +0200

    got a first (almost working version), performancewise atrocious, but it doesnt crash!

--- src/OCR-Project.cc
+++ src/OCR-Project.cc
@@ -8,10 +8,11 @@ int main(){
 //soon
 
 //read the greyscale image
-  auto pixels=boost_gil_read_img("./Testimages/7007.jpg");
+  auto pixels=boost_gil_read_img("../Testimages/7007.jpg");
 //do stuff to the image
 
 //extract glyphs from the image
+  std::cout << "got a problem here??? \n";
   auto glyphs= gly_scan(pixels);
 //write glyphs to vector
   auto  text =recognise(glyphs);
--- src/jpegimportGIL.hh
+++ src/jpegimportGIL.hh
@@ -34,18 +34,18 @@ decltype(auto) boost_gil_read_img(const std::string & fname)
 
   //instantiating the matrix
 
-  matrix pixels;
+  matrix pixels;//={ {(width)} };
   //copying the pixeldata (in case of there being some sort of proxy - thx c++)
-
   for (int i=0; i<height; ++i){
-    std::vector<int> line;
+    std::vector<int> line (width);
     for (int j=0; j<width; ++j){
-      line.push_back(*const_view(img).at(j,i)); //Orientation ok???
+      line[j]=*const_view(img).at(j,i); //Orientation ok???
     }
     pixels.push_back(line);
   }
 
-  return std::move(pixels);
+  return pixels;//std::move(pixels); Move will probably not do, as the problem size is unknown at compile time, but who knows...?
+
 
 }
 
