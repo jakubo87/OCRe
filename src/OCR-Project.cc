8bf82cebf28dea29632f0387d3afe81d531983de
jakubo87 <jakub_o@hotmail.com>
2018-09-11 22:20:08 +0200

    typos due to unexpected functionality in gnome builder....

--- src/OCR-Project.cc
+++ src/OCR-Project.cc
@@ -2,23 +2,22 @@
 #include "jpegimportGIL.hh"
 #include "gly_scan.hh"
 #include "writetofile.hh"
+#include <future>
 
 int main(){
 
 //prepare masks for recognition
 
-//read the greyscale image
+//read the gre  yscale image
   auto pixels=boost_gil_read_img("../Testimages/7007.jpg");
 //do stuff to the image
 
 //extract glyphs from the image
   auto glyphs= gly_scan(pixels);
-//write glyphs to vector
+//write glyphs to ve  ctor
   auto  text =recognise(glyphs);
 //write to file
   writetofile(text);
-
-
   return 0;
 }
 
